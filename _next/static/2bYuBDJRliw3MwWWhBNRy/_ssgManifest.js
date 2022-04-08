d7ca7c4ca339eb24cb200e629e309312fa37da44
szweibel <szweibel@users.noreply.github.com>
2022-04-08 22:04:50 +0000

    deploy: 095bf4d944f90dbf043eee2197b681f8854b4398

---
+++ _next/static/2bYuBDJRliw3MwWWhBNRy/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002FGlossary","\u002F","\u002Fauthors\u002F[slug]","\u002Fguides\u002F[slug]","\u002Finsights\u002F[slug]","\u002Fworkshops\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ images/guides/README.md
@@ -0,0 +1,3 @@
+# Installation guide images
+
+This directory should contain `v2.0` screenshots only.
