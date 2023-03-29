c955e04f0c28f4fd6884e2bb0902e52c3bb2b4ec
Stephen Zweibel <oneperstephen@gmail.com>
2023-03-29 11:25:43 -0400

    WebR

--- components/Editor/RRunner.js
+++ components/Editor/RRunner.js
@@ -1,52 +1,21 @@
 import { useRef, useEffect, useState, useContext, useReducer, Fragment } from 'react';
 import Script from 'next/script';
 import dynamic from 'next/dynamic';
+
 const EditorComponent = dynamic(() => import('./EditorComponent'), { ssr: false });
 import CloseIcon from '@mui/icons-material/Close';
 import EditorTopbar from './EditorTopbar';
+import { WebR } from '@r-wasm/webr';
 
-// examples of use of R in the browser
-// <html>
-//   <head>
-//     <title>WebR Test Console</title>
-//   </head>
-//   <body>
-//     <div>
-//       <pre><code id="out">Loading webR, please wait...</code></pre>
-//       <input spellcheck="false" autocomplete="off" id="input" type="text">
-//       <button onclick="globalThis.sendInput()" id="run">Run</button>
-//     </div>
+// Explicitly set the webR base URL to the webR npm package directory
+const webR = new WebR({
+    baseUrl: './node_modules/@r-wasm/webr/dist/',
+  });
 
-//     <script type="module">
-//       /* Create a webR console using the Console helper class */
-//       import { Console } from 'https://webr.r-wasm.org/latest/webr.mjs';
-//       const webRConsole = new Console({
-//         stdout: line => document.getElementById('out').append(line + '\n'),
-//         stderr: line => document.getElementById('out').append(line + '\n'),
-//         prompt: p => document.getElementById('out').append(p),
-//       });
-//       webRConsole.run();
-
-//       /* Write to the webR console using the ``stdin()`` method */
-//       let input = document.getElementById('input');
-//       globalThis.sendInput = () => {
-//         webRConsole.stdin(input.value);
-//         document.getElementById('out').append(input.value + '\n');
-//         input.value = "";
-//       }
-
-//       /* Send input on Enter key */
-//       input.addEventListener(
-//         "keydown",
-//         (evt) => {if(evt.keyCode === 13) globalThis.sendInput()}
-//       );
-//     </script>
-//   </body>
-// </html>
 
 export default function RRunner(props) {
     const [isRReady, setIsRReady] = useState(false);
-    const [isRLoading, setIsRLoading] = useState(false);
+    const [isRLoading, setIsRLoading] = useState(true);
     const [isError, setIsError] = useState(false);
     const [error, setError] = useState(null);
     const [isOutput, setIsoutput] = useState(false);
@@ -54,83 +23,111 @@ export default function RRunner(props) {
     const [runningCode, setRunningCode] = useState(false);
 
     const outputRef = useRef('');
-
+    const [result, setResult] = useState(['Loading webR...']);
     const code = props.code ? props.code : '';
 
-
-    useEffect(() => {
-
-        if (isRReady) {
-
-            const webRConsole = new Console({
-                stdout: line => outputRef.current += line + '\n',
-                stderr: line => outputRef.current += line + '\n',
-                prompt: p => outputRef.current += p,
-            });
-            webRConsole.run();
-            setOutput(outputRef.current);
-            setIsoutput(true);
-            setRunningCode(false);
+    async function runRCode() {
+        await webR.init();
+        const rnorm = await webR.evalR('rnorm(5,1,1)');
+        try {
+            const result = await rnorm.toArray();
+            setResult(result);
         }
-    }, [isRReady]);
+        catch (e) {
+            setError(e);
+        }
+        finally {
+            webR.destroy(rnorm);
+        }
+    }
+
+    // useEffect(() => {
+    //     if (isRReady && !isRLoading) {
+    //         runRCode();
+    //         setIsRLoading(true);
+    //     }
+    // }, [isRReady]);
+
+    // when the page is loaded, initialize webR and run the code.
+    useEffect(() => {
+        const webR = new WebR();
+         webR.init().then(() => {
+            setIsRReady(true);
+            setIsRLoading(false);
+            runRCode();
+        });
+    }, []);
+
 
 
     return (
         <Fragment>
-      {<><Script src="https://webr.r-wasm.org/latest/webr.mjs" 
-          onLoad={() => {
-            if (!isRReady && !isRLoading) {
-              async function load() {
-                globalThis.webR = new WebR();
-              }
-              load().then(() => {
-                setIsRReady(true);
-                setIsRLoading(false);
-              })
-            }
-          }}
-        /></>}
+            {/* {<><Script src="https://webr.r-wasm.org/latest/webr.mjs"
+                type='module'
+                onLoad={() => {
+                    console.log('R is loading');
+                    if (!isRReady) {
+                        async function load() {
+                            // webR = dynamic(import('https://webr.r-wasm.org/latest/webr.mjs'));
+                               // Dynamically load webR
+          const webRLoad = (await import('https://webr.r-wasm.org/latest/webr.mjs')).default;
+          const webR = new webR;
 
-          <div className="editorContainer">
-            <EditorTopbar spinnerNeeded={(isRLoading || runningCode)}
-                setCode={props.setCode}
-                setEditorOpen={props.setEditorOpen}
-                setAskToRun={props.setAskToRun}
-                setRunningCode={setRunningCode}
-                runningCode={runningCode}
-                setOutput={setOutput}
-                setIsoutput={setIsoutput}
-                setIsError={setIsError}
-                setError={setError}
-                code={code}
-            />
-            <div className="editor">
-                <div className="editorCode">
-                    <pre className='hljs'>
-                        <code className='editorCode'
-                            dangerouslySetInnerHTML={{ __html: 44 }}>
-                                
-                        </code>
-                    </pre>
-                </div>
-                <div className="editorOutput">
-                    {isError && <div className="editorOutputError">
+                        }
+                        load().then(() => {
+                            console.log('R is ready');
+                            setIsRReady(true);
+                            setIsRLoading(false);
+                        })
+                    }
+                }}
+                onError={(e) => {
+                    console.error('Script failed to load', e)
+                  }}
+            /></>} */}
+
+           
+            <div className="editorContainer">
+                <EditorTopbar spinnerNeeded={(isRLoading && !isRReady) ? true : false}
+                    setCode={props.setCode}
+                    setEditorOpen={props.setEditorOpen}
+                    setAskToRun={props.setAskToRun}
+                    setRunningCode={setRunningCode}
+                    runningCode={runningCode}
+                    setOutput={setOutput}
+                    setIsoutput={setIsoutput}
+                    setIsError={setIsError}
+                    setError={setError}
+                    code={code}
+                />
+                <div className="editor">
+                    <div className="editorCode">
                         <pre className='hljs'>
-                            <code className='editorOutputError'
-                                dangerouslySetInnerHTML={{ __html: error }}>
+                            <code className='editorCode'
+                            >
+                                {result}
+
                             </code>
                         </pre>
-                    </div>}
-                    {isOutput && <div className="editorOutputCode">
-                        <pre className='hljs'>
-                            <code className='editorOutputCode'
-                                dangerouslySetInnerHTML={{ __html: output }}>
-                            </code>
-                        </pre>
-                    </div>}
+                    </div>
+                    <div className="editorOutput">
+                        {isError && <div className="editorOutputError">
+                            <pre className='hljs'>
+                                <code className='editorOutputError'
+                                    dangerouslySetInnerHTML={{ __html: error }}>
+                                </code>
+                            </pre>
+                        </div>}
+                        {isOutput && <div className="editorOutputCode">
+                            <pre className='hljs'>
+                                <code className='editorOutputCode'
+                                    dangerouslySetInnerHTML={{ __html: output }}>
+                                </code>
+                            </pre>
+                        </div>}
+                    </div>
                 </div>
             </div>
-        </div>
         </Fragment >
     )
 }
