71d54431a6c56de5044eb3d42ee80c6615b34cc1
Stephen Zweibel <oneperstephen@gmail.com>
2023-07-05 10:53:39 -0400

    R changes

--- components/Editor/RRunner.js
+++ components/Editor/RRunner.js
@@ -2,18 +2,17 @@ import { useRef, useEffect, useState, useContext, useReducer, Fragment } from 'r
 import Script from 'next/script';
 import dynamic from 'next/dynamic';
 const EditorComponent = dynamic(() => import('./EditorComponent'), { ssr: false });
+import CloseIcon from '@mui/icons-material/Close';
 import EditorTopbar from './EditorTopbar';
 import { WebR } from '@r-wasm/webr';
 
 // Explicitly set the webR base URL to the webR npm package directory
-const webR = new WebR(
-//     {
-//     baseUrl: '../node_modules/@r-wasm/webr/dist/',
-// }
-);
+const webR = new WebR();
+webR.init();
 
 
-export default function RRunner(props) {
+export default function RRunner({ defaultCode, minLines, codeOnChange, ...props }) {
+    const startingCode = props.text;
     const [isRReady, setIsRReady] = useState(false);
     const [isRLoading, setIsRLoading] = useState(true);
     const [isError, setIsError] = useState(false);
@@ -21,65 +20,49 @@ export default function RRunner(props) {
     const [isOutput, setIsoutput] = useState(false);
     const [output, setOutput] = useState('');
     const [runningCode, setRunningCode] = useState(false);
-
+    const [code, setCodeState] = useState(startingCode);
     const outputRef = useRef('');
     const [result, setResult] = useState(['Loading webR...']);
-    const code = props.code ? props.code : '';
 
-    async function runRCode() {
-        await webR.init();
-        const rnorm = await webR.evalR('rnorm(5,1,1)');
-        try {
-            console.log('rnorm', rnorm);
-            const result = await rnorm.toArray();
-            setResult(result);
-            console.log(result);
-        }
-        catch (e) {
-            setError(e);
-        }
-        finally {
-            webR.destroy(rnorm);
-        }
-    }
 
-    // when the page is loaded, initialize webR and run the code.
+    const onChange = (newValue) => {
+        setCodeState(newValue);
+    };
+
+
     useEffect(() => {
-            runRCode();
+        setIsRLoading(false);
+        setIsRReady(true);
     }, []);
 
 
+    async function runR() {
+        const shelter = await new webR.Shelter();
+        const result = await shelter.captureR(code, {
+            withAutoprint: true,
+            captureStreams: true,
+            captureConditions: false
+        });
+        try {
+            const out = result.output.filter(
+                evt => evt.type == 'stdout' || evt.type == 'stderr'
+            ).map((evt) => evt.data);
+            //   document.getElementById('out').innerText = out.join('\n');
+            setOutput(out.join('\n'));
+        } finally {
+            shelter.purge();
+        }
+    }
+
+
+    const height = props.height ? props.height : '100%';
 
     return (
         <Fragment>
-            {/* {<><Script src="https://webr.r-wasm.org/latest/webr.mjs"
-                type='module'
-                onLoad={() => {
-                    console.log('R is loading');
-                    if (!isRReady) {
-                        async function load() {
-                            // webR = dynamic(import('https://webr.r-wasm.org/latest/webr.mjs'));
-                               // Dynamically load webR
-          const webRLoad = (await import('https://webr.r-wasm.org/latest/webr.mjs')).default;
-          const webR = new webR;
-
-                        }
-                        load().then(() => {
-                            console.log('R is ready');
-                            setIsRReady(true);
-                            setIsRLoading(false);
-                        })
-                    }
-                }}
-                onError={(e) => {
-                    console.error('Script failed to load', e)
-                  }}
-            /></>} */}
-
-
             <div className="editorContainer">
                 <EditorTopbar spinnerNeeded={(isRLoading && !isRReady) ? true : false}
                     setCode={props.setCode}
+                    run={runR}
                     setEditorOpen={props.setEditorOpen}
                     setAskToRun={props.setAskToRun}
                     setRunningCode={setRunningCode}
@@ -90,34 +73,94 @@ export default function RRunner(props) {
                     setError={setError}
                     code={code}
                 />
-                <div className="editor">
-                    <div className="editorCode">
-                        <pre className='hljs'>
-                            <code className='editorCode'
-                            >
-                                {result}
-
-                            </code>
-                        </pre>
-                    </div>
-                    <div className="editorOutput">
-                        {isError && <div className="editorOutputError">
-                            <pre className='hljs'>
-                                <code className='editorOutputError'
-                                    dangerouslySetInnerHTML={{ __html: error }}>
-                                </code>
-                            </pre>
-                        </div>}
-                        {isOutput && <div className="editorOutputCode">
-                            <pre className='hljs'>
-                                <code className='editorOutputCode'
-                                    dangerouslySetInnerHTML={{ __html: output }}>
-                                </code>
-                            </pre>
-                        </div>}
-                    </div>
-                </div>
+                <EditorComponent code={code}
+                    onChange={onChange}
+                    maxLines='Infinity'
+                    minLines={minLines}
+                    height={height} />
             </div>
+
+            {isError && <div id="error"
+                style={{
+                    font: "1.3rem Inconsolata, monospace",
+                    margin: "10px",
+                    padding: "10px",
+                    border: "1px solid red",
+                    borderRadius: "5px",
+                    backgroundColor: "#f5f5f5",
+                    color: "red",
+                    fontSize: "20px",
+                    overflow: "auto",
+                    whiteSpace: "pre-wrap"
+                }}>
+                <CloseIcon
+                    onClick={closeError}
+                    style={{
+                        float: "right",
+                        fontSize: "20px",
+                        color: "#32c259",
+                        marginRight: "10px",
+                        cursor: "pointer"
+                    }}
+                />
+                {String(error)}
+            </div>}
+            <div id="output"
+                style={{
+                    font: "1.3rem Inconsolata, monospace",
+                    margin: "10px",
+                    padding: "10px",
+                    border: "1px solid #32c259",
+                    borderRadius: "5px",
+                    backgroundColor: "#f5f5f5",
+                    color: "#32c259",
+                    fontSize: "20px",
+                    overflow: "auto",
+                    whiteSpace: "pre-wrap"
+                }}>
+                <CloseIcon
+                    // onClick={closeOutput}
+                    style={{
+                        float: "right",
+                        fontSize: "20px",
+                        color: "#32c259",
+                        marginRight: "10px",
+                        cursor: "pointer"
+                    }}
+                />
+                {output}
+            </div>
+            <div id="result"
+                style={{
+                    font: "1.3rem Inconsolata, monospace",
+                    margin: "10px",
+                    padding: "10px",
+                    border: "1px solid #32c259",
+                    borderRadius: "5px",
+                    backgroundColor: "#f5f5f5",
+                    color: "#32c259",
+                    fontSize: "20px",
+                    overflow: "auto",
+                    whiteSpace: "pre-wrap"
+                }}>
+                <CloseIcon
+                    // onClick={closeResult}
+                    style={{
+                        float: "right",
+                        fontSize: "20px",
+                        color: "#32c259",
+                        marginRight: "10px",
+                        cursor: "pointer"
+                    }}
+                />
+                {result}
+            </div>
+
+            <div id="fig" style={{
+                width: "100%",
+                height: "100%",
+                display: "none"
+            }}></div>
         </Fragment >
     )
 }
