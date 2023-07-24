cce73a9acaccbac0e35442e81c809679e2eb1615
Stephen Zweibel <oneperstephen@gmail.com>
2023-07-24 14:52:08 -0400

    service worker?

--- components/Webvm.js
+++ components/Webvm.js
@@ -1,8 +1,17 @@
 import EditorTopbar from './Editor/EditorTopbar';
+import Script from 'next/script';
+
 export default function Webvm(props) {
     return (
         
         <div className='webvm'>
+            <Script
+                strategy='beforeInteractive'
+                onLoad={() => {
+                    setIsRLoading(false);
+                    setIsRReady(true);
+                }}
+                src='../../coi-service.js' />
         <EditorTopbar {...props} />    
             <iframe src='../../webvm/index.html' width='100%' height='100%'></iframe>
         </div>
