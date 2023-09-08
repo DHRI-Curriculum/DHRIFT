8fce6ac0e304753fce0307bf10b0d032d3241f35
Stephen Zweibel <oneperstephen@gmail.com>
2023-09-08 14:51:44 -0400

    still working on SWR

--- pages/index.js
+++ pages/index.js
@@ -7,43 +7,10 @@ import logo from '../public/images/logos/logo.png'
 import Image from 'next/image'
 import useSWR from 'swr'
 
-let workshopsBuiltURL, headers;
-if (process.env.NEXT_PUBLIC_GITHUBSECRET === 'true') {
-  headers = new Headers(
-    {
-      'Content-Type': 'application/json',
-      'authorization': `token ${process.env.NEXT_PUBLIC_GITHUBSECRET}`
-    });
-} else {
-  headers = new Headers(
-    {
-      'Content-Type': 'application/json',
-    });
-}
-
-const workshopFetcher = (headers) => (...args) => fetch(...args, {
-  headers: headers,
-  method: 'GET',
-}).then(
-  res => res.json()
-).then(
-  // decode from base64
-  res => Buffer.from(res.content, 'base64').toString()
-)
-
-function useWorkshop(workshop){
-  const { data, error } = useSWR(workshop.url, workshopFetcher(headers),
-    { revalidateOnFocus: false, revalidateOnReconnect: false });
-  return data;
-}
-
 export default function Home() {
 
   const [gitUser, setGitUser] = useState(null);
   const [gitRepo, setGitRepo] = useState(null);
-
-
-
 
   const allFetcher = (headers) => (...args) => fetch(...args, {
     headers: headers,
@@ -54,11 +21,41 @@ export default function Home() {
     err => console.log('err', err)
   )
 
+  let workshopsBuiltURL, headers;
   workshopsBuiltURL = `https://api.github.com/repos/${gitUser}/${gitRepo}/contents/`
   const { data: allWorkshops, isLoading, error } = useSWR(gitUser ? workshopsBuiltURL : null, allFetcher(headers),
-    { revalidateOnFocus: false, revalidateOnReconnect: false });
+  { revalidateOnFocus: false, revalidateOnReconnect: false });
   console.log('allWorkshops', allWorkshops);
+
+  if (process.env.NEXT_PUBLIC_GITHUBSECRET === 'true') {
+    headers = new Headers(
+      {
+        'Content-Type': 'application/json',
+        'authorization': `token ${process.env.NEXT_PUBLIC_GITHUBSECRET}`
+      });
+  } else {
+    headers = new Headers(
+      {
+        'Content-Type': 'application/json',
+      });
+  }
+
+  const workshopFetcher = (headers) => (...args) => fetch(...args, {
+    headers: headers,
+    method: 'GET',
+  }).then(
+    res => res.json()
+  ).then(
+    // decode from base64
+    res => Buffer.from(res.content, 'base64').toString()
+  )
   
+  function useWorkshop(workshop){
+    const { data, error } = useSWR(workshop.url, workshopFetcher(headers),
+      { revalidateOnFocus: false, revalidateOnReconnect: false });
+    return data;
+  }
+
   let workshops
   allWorkshops && allWorkshops.map(workshop => {
     useWorkshop(workshop);
