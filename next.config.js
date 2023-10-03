a75481041cc5ca746047e22afa3b8073c6a047b7
Stephen Zweibel <oneperstephen@gmail.com>
2023-10-03 15:03:23 -0400

    bit more

--- next.config.js
+++ next.config.js
@@ -20,23 +20,6 @@ process.env.NEXT_PUBLIC_GITHUB_ACTIONS = process.env.GITHUB_ACTIONS || false;
 process.env.NEXT_PUBLIC_GITHUBSECRET = process.env.GITHUBSECRET || false;
 
 
-
-
-// const imagesConfig = function(build, isGitHub) {
-//     if (isGitHub) {
-//         return {
-//             unoptimized: true,
-//             // path: '../..',
-//         }
-//     } 
-//     else if (build) {
-//         return {
-//             unoptimized: true,
-//             // path: '../..',
-//         }
-//     }
-// }
-
 const nextConfig = {
     output: "export",
     trailingSlash: true,
@@ -45,23 +28,6 @@ const nextConfig = {
     images: {
         unoptimized: true,
     }
-    // async headers() {
-    //     return [
-    //         {
-    //             source: '/(.*)',
-    //             headers: [
-    //                 {
-    //                     key: 'Cross-Origin-Opener-Policy',
-    //                     value: 'same-origin',
-    //                 },
-    //                 {
-    //                     key: 'Cross-Origin-Embedder-Policy',
-    //                     value: 'require-corp',
-    //                 },
-    //             ],
-    //         },
-    //     ]
-    // }
 }
 
 console.log(nextConfig);
