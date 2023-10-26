f36d969e507c440313117bec7adcdbbd8a11e0f9
Stephen Zweibel <oneperstephen@gmail.com>
2023-10-26 09:32:45 -0400

    no jupyter locally

--- components/Wasm/Webvm.js
+++ components/Wasm/Webvm.js
@@ -9,17 +9,6 @@ export default function Webvm(props) {
             <Head>
         <script src='/coi-service.js' async></script>
             </Head>
-            {/* <Script
-                id='coi-service'
-                strategy='beforeInteractive'
-                onLoad={() => {
-                    console.log('COI loaded')
-                }}
-                onError={(err) => {
-                    console.log('COI error', err)}}
-
-                src={'../coi-service.js'}
-            /> */}
             <EditorTopbar {...props} />
             <iframe src='../../webvm/index.html' width='100%' height='95%'></iframe>
         </div>
--- public/jupyterlite/files/README.md
+++
@@ -1,6 +0,0 @@
-## Further Information and Updates
-
-For more info, keep an eye on the JupyterLite documentation:
-
-- How-to Guides: https://jupyterlite.readthedocs.io/en/latest/howto/index.html
-- Reference: https://jupyterlite.readthedocs.io/en/latest/reference/index.html
