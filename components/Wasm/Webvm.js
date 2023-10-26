0b98d15a1efb931fa26fdc21cd9fe95241eb2daf
Stephen Zweibel <oneperstephen@gmail.com>
2023-10-26 12:01:56 -0400

    COI back

--- components/Wasm/Webvm.js
+++ components/Wasm/Webvm.js
@@ -1,13 +1,12 @@
 import EditorTopbar from '../Editor/EditorTopbar';
 import Script from 'next/script';
-// import COIService from '../../public/coi-service.js';
 import Head from 'next/head';
 
 export default function Webvm(props) {
     return (
         <div className='webvm'>
             <Head>
-        {/* <script src='/coi-serviceworker.js' async></script> */}
+        <script src='/coi-serviceworker.js' async></script>
             </Head>
             <EditorTopbar {...props} />
             <iframe src='../../webvm/index.html' width='100%' height='95%'></iframe>
