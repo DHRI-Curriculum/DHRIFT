357406503bf4e4bac8c7a61c2e8de79022c265ea
Stephen Zweibel <oneperstephen@gmail.com>
2024-04-01 15:17:20 -0400

    Add dependencies and update form.js

--- pages/dynamic/form.js
+++ pages/dynamic/form.js
@@ -1,10 +1,20 @@
 import Header from '../../components/Header';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Octokit } from '@octokit/rest';
+import { Button } from '@mui/material';
+import { useEffect } from 'react';}
 
 export default function Form() {
     const [user, setUser] = useState("");
     const [repo, setRepo] = useState("");
     const [file, setFile] = useState("");
+
+    useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+    console.log(token);
+    }
+    , []);
 
     const handleSubmit = (evt) => {
         evt.preventDefault();
@@ -17,8 +27,21 @@ export default function Form() {
         const fileName = file.endsWith('.md') ? file.slice(0, -3) : file;
         // redirect to /dynamic/?user=${username}&repo=${repoName}&file=${fileName}
         window.location.href = `/dynamic/?user=${username}&repo=${repoName}&file=${fileName}`
-
     }
+
+    // const testSubmit = async () => {
+    //     const response = await octokit.rest.repos.createForAuthenticatedUser({
+    //         name: 'hello-world',
+    //         description: 'This is your first repository',
+    //     });
+    //     console.log(response);
+    // }
+
+    const testSubmit = async () => {
+        const permRequest =  'https://github.com/login/oauth/authorize?scope=repo&client_id=b5be98ebcdc9cdf67526&state=%7B%22owner%22%3A%20%22smythp%22%7D';
+        window.location.href = permRequest;
+    }
+
 
 
     return (
@@ -54,6 +77,9 @@ export default function Form() {
             </label>
             <input type="submit" value="Submit" />
         </form>
+        
+        <Button variant="contained" onClick={testSubmit}>Submit</Button>
+
         </>
     );
 }
