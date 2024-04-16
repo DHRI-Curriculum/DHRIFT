200edd12df4c4dc458063d9662ef101794a3ef76
Stephen Zweibel <oneperstephen@gmail.com>
2024-04-16 10:38:36 -0400

    Add form and registration descriptions to the landing page

--- pages/dynamic/form.js
+++ pages/dynamic/form.js
@@ -113,18 +113,48 @@ export default function Form() {
         });
     };
 
+    const formDescription = (
+        <div>
+            <h2>Create a DHRIFT Community Landing Page</h2>
+            <p>The following form can be used to generate a landing page for organizing and leading workshops based on DHRIFT’s menu of workshops. By completing the fields below, you will produce a web page that can be used to organize a digital humanities learning event, which might include a single workshop on one day or scale to a multi-day event with multiple instructors, workshops, and even locations. For examples, please see the DHRIFT Gallery. Once generated, the resulting page will display the information you provide below.</p>
+
+            <h2>Getting Started</h2>
+            <p>The wizard will require you to log into your GitHub account, and the generated page will live as a github page in your git-hub account; however, all of the workshops you add will continue to be hosted on the DHRIFT site.</p>
+            <p>The information you provide below will be stored in a file in your repository, and the resulting page will be hosted on GitHub Pages.</p>
+            <p>
+                NOTE: You may use this page to update previous information on your DHRIFT landing page; however, this form currently overwrites all of the content on any previous DHRIFT page you may have saved in the same location.
+                What you will need:
+                Basic information about your institute
+                Dates and times for workshops and events
+                Names of instructors and helpers
+                A github account and all of your login information on hand
+
+                For more information about how to make the most out of your DHRIFT landing page, please see our documentation here… wherever we do that. </p>
+
+
+        </div>
+    );
+
+    const registrationDescription = (
+        <div>
+            <h2>Participant Application and / or Registration Information</h2>
+            <p>You can customize your DHRIFT landing page to include links to an application or registration page. DHRIFT does not include a registration page. We recommend that you use another service, such as a Google form or Zoom registration; however, you can turn this section on or off here. Any information provided in this section will appear directly below the event description.</p>
+        </div>
+    );
+
     return (
         <>
             <Header title={'Dhrift'} instUser={'dhri-curriculum'} instRepo={'dhrift-site-template'}
                 gitUser={'dhri-curriculum'} gitRepo={'workshops'}
             />
             <Container>
-                <h1>Create your own DHRIFT Institute</h1>
+                {formDescription}
                 {instCreated &&
                     instCreatedSuccess
                 }
                 {!instCreated && (
                     <form>
+                        {registrationDescription}
                         <Stack spacing={2}>
                             <TextField label="Register Text" type="text" name="registerText" value={formData.registerText} onChange={handleInputChange} />
                             <TextField label="Venue" type="text" name="venue" value={formData.venue} onChange={handleInputChange} />
@@ -157,13 +187,13 @@ export default function Form() {
                                 <div key={index}>
                                     <h4>Session {index + 1}</h4>
                                     <Stack spacing={2}>
-                                    <TextField label="Date" type="date" value={session.date} onChange={(e) => handleArrayFieldChange('sessions', index, 'date', e.target.value)} />
-                                    <TextField label="Time" type="time" value={session.time} onChange={(e) => handleArrayFieldChange('sessions', index, 'time', e.target.value)} />
-                                    <TextField label="Title" type="text" value={session.title} onChange={(e) => handleArrayFieldChange('sessions', index, 'title', e.target.value)} />
-                                    <TextField label="Description" type="text" value={session.description} onChange={(e) => handleArrayFieldChange('sessions', index, 'description', e.target.value)} />
-                                    {formData.sessions.length > 1 && (
-                                        <Button type="button" onClick={() => handleRemove('sessions', index)}>Remove Session</Button>
-                                    )}
+                                        <TextField label="Date" type="date" value={session.date} onChange={(e) => handleArrayFieldChange('sessions', index, 'date', e.target.value)} />
+                                        <TextField label="Time" type="time" value={session.time} onChange={(e) => handleArrayFieldChange('sessions', index, 'time', e.target.value)} />
+                                        <TextField label="Title" type="text" value={session.title} onChange={(e) => handleArrayFieldChange('sessions', index, 'title', e.target.value)} />
+                                        <TextField label="Description" type="text" value={session.description} onChange={(e) => handleArrayFieldChange('sessions', index, 'description', e.target.value)} />
+                                        {formData.sessions.length > 1 && (
+                                            <Button type="button" onClick={() => handleRemove('sessions', index)}>Remove Session</Button>
+                                        )}
                                     </Stack>
                                 </div>
                             ))}
