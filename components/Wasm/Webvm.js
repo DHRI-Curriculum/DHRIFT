11cb67bd8d8757c2134bedeb61bad82690ee1be8
Stephen Zweibel <oneperstephen@gmail.com>
2024-09-11 11:29:06 -0400

    refactor: Update PAGE_URL environment variable in nextjs.yml
    The PAGE_URL environment variable in the nextjs.yml workflow file was updated to use the base_path output from the setup-pages step. This change ensures that the correct page URL is used when building and exporting the Next.js application.

--- components/Wasm/Webvm.js
+++ components/Wasm/Webvm.js
@@ -3,10 +3,11 @@ import Script from 'next/script';
 import Head from 'next/head';
 
 export default function Webvm(props) {
+    
     return (
         <div className='webvm'>
             <Head>
-                <script src='/coi-serviceworker.js' async></script>
+                <script src={process.env.NEXT_PUBLIC_ASSET_PREFIX +'/coi-serviceworker.js'} async></script>
             </Head>
             {/* <EditorTopbar {...props} /> */}
             <iframe src='../../webvm/index.html' width='100%' height='95%' id="iframe"></iframe>
