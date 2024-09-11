9f052bc22d4f8eff9317febf086c390ae1fda8f2
Stephen Zweibel <oneperstephen@gmail.com>
2024-09-11 12:52:01 -0400

    refactor: Update iframe source URL in Webvm.js

--- components/Wasm/Webvm.js
+++ components/Wasm/Webvm.js
@@ -9,9 +9,7 @@ export default function Webvm(props) {
             <Head>
                 <script src={process.env.NEXT_PUBLIC_ASSET_PREFIX +'/coi-serviceworker.js'} async></script>
             </Head>
-            {/* <EditorTopbar {...props} /> */}
             <iframe src='./webvm/index.html' width='100%' height='95%' id="iframe"></iframe>
-            {/* <iframe src='../../webvm/index.html' width='100%' height='95%' id="iframe"></iframe> */}
-        </div>
+           </div>
     )
 }
