688e8e8255f5c07d27b77666601de19baaaccb0e
Stephen Zweibel <oneperstephen@gmail.com>
2024-09-11 11:06:14 -0400

    refactor: Update favicon URLs to use the NEXT_PUBLIC_ASSET_PREFIX environment variable
    The favicon URLs in the _document.js file were updated to use the NEXT_PUBLIC_ASSET_PREFIX environment variable. This change ensures that the correct favicon paths are used when the application is deployed. Additionally, the previous hard-coded favicon paths were commented out for reference.

--- pages/_document.js
+++ pages/_document.js
@@ -6,18 +6,20 @@ class MyDocument extends Document {
     return { ...initialProps }
   }
 
+  
   render() {
+    const favIconLocation = process.env.NEXT_PUBLIC_ASSET_PREFIX + "/favicon.ico";
     return (
       <Html>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-        {process.env.NEXT_PUBLIC_GITHUB_ACTIONS == false && <><link rel="icon" href={"/favicon.ico"} sizes="32x32"></link>
-          <link rel="icon" href={"/favicon.ico"} sizes="192x192"></link>
-          <link rel="apple-touch-icon" href={"/favicon.ico"}></link></>
+        {process.env.NEXT_PUBLIC_GITHUB_ACTIONS == false && <><link rel="icon" href={favIconLocation} sizes="32x32"></link>
+          <link rel="icon" href={favIconLocation} sizes="192x192"></link>
+          <link rel="apple-touch-icon" href={favIconLocation}></link></>
           ||
-          <><link rel="icon" href={"/favicon.ico"} sizes="32x32"></link>
-            <link rel="icon" href={"/favicon.ico"} sizes="192x192"></link>
-            <link rel="apple-touch-icon" href={"/favicon.ico"}></link></>}
+          <><link rel="icon" href={favIconLocation} sizes="32x32"></link>
+            <link rel="icon" href={favIconLocation} sizes="192x192"></link>
+            <link rel="apple-touch-icon" href={favIconLocation}></link></>}
         <Head />
         <body>
           <Main />
