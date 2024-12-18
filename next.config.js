7d1975e4d30d85446ebd9e834f2e63315b07d701
Stephen Zweibel <oneperstephen@gmail.com>
2024-12-18 18:32:17 -0500

    feat: Add SCSS support by configuring sass-loader and updating package dependencies

--- next.config.js
+++ next.config.js
@@ -25,6 +25,15 @@ const nextConfig = {
             test: /\.css$/,
             use: ['style-loader', 'css-loader']
         });
+        // SCSS handling
+        config.module.rules.push({
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        });
         return config;
     }
 }
